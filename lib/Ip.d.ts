294b85c26440c4d524be0de86900c0be3386e99e
xlaez <utibeabasiekong2@gmail.com>
2023-07-26 20:47:33 +0100

    fix: issues with ip module and esnext

---
+++ lib/Ip.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+export function getAddress(): os.NetworkInterfaceInfoIPv4;
+export function getIpAdress(): string;
+export function getMacAddress(): string;
+import os = require("os");
+//# sourceMappingURL=Ip.d.ts.map
