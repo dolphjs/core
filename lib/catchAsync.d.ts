58b41a1b0345a35637db66f2743e22519d57f1c9
xlaez <utibeabasiekong2@gmail.com>
2023-07-26 20:46:19 +0100

    fix: issues with ip module and esnext

--- lib/catchAsync.d.ts
+++
@@ -1,8 +0,0 @@
-export = catchAsync;
-/**
- *
- * @param {Function} fn
- * @returns {(fn: Function) => (req: import("express").Request, res: import("express").Response, next: import("express").NextFunction) => void}
- */
-declare function catchAsync(fn: Function): (fn: Function) => (req: import("express").Request, res: import("express").Response, next: import("express").NextFunction) => void;
-//# sourceMappingURL=catchAsync.d.ts.map
